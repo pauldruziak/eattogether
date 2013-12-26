efb8a584d8e62a2d16b82cdc9a91aaae681f4579
Pavel Druzyak <pavel.druzyak@gmail.com>
2013-12-27 01:45:13 +0200

    Add sceanario for successful creating Event

--- features/step_definitions/event_steps.rb
+++ features/step_definitions/event_steps.rb
@@ -1,3 +1,13 @@
 When(/^я спробую створити подію$/) do
   visit new_event_path
 end
+
+When(/^я створю подію з вірними даними$/) do
+  visit new_event_path
+  fill_in 'event[title]', with: 'My first event'
+  click_on 'Create'
+end
+
+Then(/^я зможу її побачити$/) do
+  expect(page).to have_content('My first event')
+end
--- features/step_definitions/session_steps.rb
+++ features/step_definitions/session_steps.rb
@@ -1,4 +1,9 @@
-Given /^я не авторизований користувач$/ do
+Given(/^я увійшов у систему$/) do
+  @user = create :user
+  visit new_user_session_path
+  fill_in 'user[email]', with: @user.email
+  fill_in 'user[password]', with: @user.password
+  click_on 'Sign in'
 end
 
 Then(/^маю побачити сторінку авторизації$/) do
