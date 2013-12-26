d150ab9ee6de04e47699c68f992eaae61f200157
Pavel Druzyak <pavel.druzyak@gmail.com>
2013-12-27 01:44:46 +0200

    Add test for EventsController#new

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe EventsController do
+  let(:user) { create :user }
+  before(:each) { sign_in user }
 
+  describe 'GET #new' do
+    it 'respond with success' do
+      get :new
+      expect(response).to be_success
+    end
+
+    it 'render new.html.slim template' do
+      get :new
+      expect(response).to render_template('new')
+    end
+
+    it 'assign new Event to @event' do
+      get :new
+      expect(assigns[:event]).to be_kind_of(Event)
+    end
+  end
 end
