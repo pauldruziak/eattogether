345107d3ebd80d552c70bb112853204dd89928bb
Pavel Druziak <pavel.druzyak@gmail.com>
2014-02-22 12:02:39 +0200

    Design for devise pages

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'sprockets/railtie'
-# require "rails/test_unit/railtie"
+# require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,7 +18,7 @@ module Eattogether
     # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # Run 'rake -D time' for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
@@ -26,7 +26,9 @@ module Eattogether
     # config.i18n.default_locale = :de
 
     config.to_prepare do
-      Devise::SessionsController.layout "session"
+      Devise::SessionsController.layout 'session'
+      Devise::PasswordsController.layout 'session'
+      Devise::RegistrationsController.layout proc { |controller| user_signed_in? ? 'application' : 'session' }
     end
   end
 end
