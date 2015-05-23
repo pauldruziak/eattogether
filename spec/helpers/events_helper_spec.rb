8ccc8f6dfc98198d69b9cf1d96366381d644d625
Pavel Druziak <pavel.druzyak@gmail.com>
2015-05-23 23:17:26 +0300

    Formatting amount, 2 number after dot

--- spec/helpers/events_helper_spec.rb
+++ spec/helpers/events_helper_spec.rb
@@ -8,7 +8,7 @@ describe EventsHelper do
         double('participant2', family_name: 'foo', amount: 10),
         double('participant3', family_name: 'bar', amount: 10)
       ]
-      expect(helper.group_by_family(participants)).to eq({ 'foo' => 20, 'bar' => 10 })
+      expect(helper.group_by_family(participants)).to eq({ 'foo' => '20.00', 'bar' => '10.00' })
     end
   end
 end
